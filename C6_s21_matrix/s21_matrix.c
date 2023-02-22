1f60530384d83586512d328f090a571fe44b7382
Fenix Necromancer <fenixnec@mi-w6.msk.21-school.ru>
2023-02-22 09:09:19 +0300

    school struct of functions matrix.h

--- C6_s21_matrix/s21_matrix.c
+++ C6_s21_matrix/s21_matrix.c
@@ -1,58 +1,34 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-typedef struct matrix_struct {
-    double** matrix;
-    int rows;
-    int columns;
-} matrix_t;
-
-int s21_create_matrix(int rows, int columns, matrix_t *result){
-    int err = 0;
-    if (rows < 0 || columns < 0) {
-        err = 1;
-    } else {
-        result->rows = rows;
-        result->columns = columns;
-        result->matrix = calloc(rows, sizeof(double*));
-        err = 1;
-        if (result->matrix != NULL) {
-            for (int i=0; i < result->rows; i++) {
-                result->matrix[i] = calloc(columns, sizeof(double));
-            }
-            err=0;
-        }
-    }
-
-    return err;
-}
-
-void print_matrix(matrix_t *mat) {
-    for(int i=0; i < mat->rows; i++) {
-        for(int j=0; j < mat->columns; j++) {            
-            printf("%.1f ", mat->matrix[i][j]);
-        }
-        printf("\n");
-    }
-}  
-
-void fill_rand(matrix_t *mat) {
-    for(int i=0; i < mat->rows; i++) {
-        for(int j=0; j < mat->columns; j++) {            
-            mat->matrix[i][j] = rand()%50;
-        }
-    }
-}
+#include "s21_matrix.h"
 
 int main() {
 
-    matrix_t exe;
-    s21_create_matrix(3, 3, &exe);
-    fill_rand(&exe);
-    print_matrix(&exe);
+  matrix_t exe;
+  s21_create_matrix(3, 3, &exe);
+  fill_rand(&exe);
+  print_matrix(&exe);
 
-    fill_rand(&exe);
-    print_matrix(&exe);
-    
-    return 0;
+  fill_rand(&exe);
+  print_matrix(&exe);
+
+  return 0;
+}
+
+int s21_create_matrix(int rows, int columns, matrix_t *result) {
+  int err = 0;
+  if (rows < 0 || columns < 0) {
+    err = 1;
+  } else {
+    result->rows = rows;
+    result->columns = columns;
+    result->matrix = calloc(rows, sizeof(double *));
+    err = 1;
+    if (result->matrix != NULL) {
+      for (int i = 0; i < result->rows; i++) {
+        result->matrix[i] = calloc(columns, sizeof(double));
+      }
+      err = 0;
+    }
+  }
+
+  return err;
 }
---
+++ C6_s21_matrix/s21_matrix.h
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SUCCESS 1
+#define FAILURE 0
+
+typedef struct matrix_struct {
+  double **matrix;
+  int rows;
+  int columns;
+} matrix_t;
+
+// matrix operations
+int s21_create_matrix(int rows, int columns, matrix_t *result);
+void s21_remove_matrix(matrix_t *A); // Очистка матриц (remove_matrix)
+int s21_eq_matrix(matrix_t *A, matrix_t *B); // Сравнение матриц (eq_matrix)
+int s21_sum_matrix(matrix_t *A, matrix_t *B, matrix_t *result); // Сложение
+int s21_sub_matrix(matrix_t *A, matrix_t *B, matrix_t *result); // Bычитание
+// Умнож-е на число
+int s21_mult_number(matrix_t *A, double number, matrix_t *result);
+// Умнож-е двух матриц
+int s21_mult_matrix(matrix_t *A, matrix_t *B, matrix_t *result);
+// Транспонирование матрицы (transpose)
+int s21_transpose(matrix_t *A, matrix_t *result);
+// Минор матрицы и матрица алгебраических дополнений
+int s21_calc_complements(matrix_t *A, matrix_t *result);
+// Определитель матрицы (determinant)
+int s21_determinant(matrix_t *A, double *result);
+// Обратная матрица (inverse_matrix)
+int s21_inverse_matrix(matrix_t *A, matrix_t *result);
+
+// helpers
+void print_matrix(matrix_t *mat);
+void fill_rand(matrix_t *mat);
